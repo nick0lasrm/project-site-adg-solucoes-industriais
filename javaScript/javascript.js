17a565372c692868ee96e32ebeaacc3c8bbb6864
Nickolas Machado <nickolasraphael3232@gmail.com>
2025-05-20 16:41:28 -0300

    att sections, more features btn back_top, animation open and close menu.

--- javaScript/javascript.js
+++ javaScript/javascript.js
@@ -1,18 +1,29 @@
 var btn_menu = document.querySelector('#btn_menu')
 var mobile_menu = document.getElementById('mobile_menu')
+const btn = document.getElementById('btn_menu')
 
-btn_menu.addEventListener('click', function(){
+btn_menu.addEventListener('click', function () {
     mobile_menu.classList.toggle('expand_menu')
 
 })
 
-window.addEventListener('scroll', function () {
-    if (window.scrollY > 400 ){
+function animar() {
+    btn.classList.toggle('ativar')
+}
+
+function closemenu() {
+    btn.classList.remove('ativar')
     mobile_menu.classList.remove('expand_menu');
+}
+
+
+window.addEventListener('scroll', function () {
+    let back_top = document.querySelector('.back_top')
+    back_top.classList.toggle('active', window.scrollY > 700)
+
+    if (window.scrollY > 400) {
+        mobile_menu.classList.remove('expand_menu');
+        btn.classList.remove('ativar');
     }
 })
 
-function animar(){  
-    const btn = document.getElementById('btn_menu')
-        btn.classList.toggle('ativar')
-}
